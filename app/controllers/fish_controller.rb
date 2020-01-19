28b6cae632af5479f7597aca4f61402f50cdd8c8
Eitan Bar-David <eitansbd@gmail.com>
2020-01-19 20:18:20 +0000

    order index by common name

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -4,7 +4,7 @@ class FishController < ApplicationController
     if page_number.match? (/\A[\d]+\z/)
       items_per_page = 10
       offset = items_per_page * (page_number.to_i - 1)
-      @fish = Fish.limit(items_per_page).offset(offset)
+      @fish = Fish.order(:common_name).limit(items_per_page).offset(offset)
       json_response(@fish)
     else
       error = "invalid page param"
