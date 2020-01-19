e94a69de418a614abc0512bcf83047f6334f935f
Eitan Bar-David <eitansbd@gmail.com>
2020-01-19 21:15:47 +0000

    change case_sensitive to false

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -2,7 +2,8 @@ class Fish < ApplicationRecord
   validates :common_name, presence: true
   
   validates :species_name, presence: true,
-                           uniqueness: true,
+                           uniqueness: { case_sensetive: false, 
+                                         message: "species has already been added"},
                            format: { with: /\A[a-z]+\.? [a-z]+\z/i,
                                      message: "must be a valid species name"}
                            
